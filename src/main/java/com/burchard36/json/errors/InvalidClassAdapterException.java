60791e7e09f8c9d9e5a877e85a48a1f4fb096551
Dalton Burchard <48583030+Burchard36@users.noreply.github.com>
2021-11-07 21:47:22 -0800

    Added InvalidCLassAdapterException when calling fromJson

---
+++ src/main/java/com/burchard36/json/errors/InvalidClassAdapterException.java
@@ -0,0 +1,7 @@
+package com.burchard36.json.errors;
+
+public class InvalidClassAdapterException extends Exception{
+    public InvalidClassAdapterException(final String message) {
+        super(message);
+    }
+}
