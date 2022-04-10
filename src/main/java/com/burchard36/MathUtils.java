a5874c54bc8cb89d329da5ce5a4db8077bb19263
Dalton <48583030+Burchard36@users.noreply.github.com>
2022-04-09 23:54:46 -0700

    Added MathUtils
    Provided by frostalf: https://github.com/frostalf

---
+++ src/main/java/com/burchard36/MathUtils.java
@@ -0,0 +1,61 @@
+package com.burchard36;
+
+/**
+ * THIS CLASS WAS PROVIDED BY ANOTHER DEVELOPER NOT ME!
+ *
+ * These math methods are faster to work with than typical Math classes, as they work
+ * with raw bits
+ *
+ * Users GitHub who provided these methods: https://github.com/frostalf
+ */
+public class MathUtils {
+
+    public static int multiply(int n1, int n2) {
+        int a = n1, b = n2, result=0;
+        while (b != 0) // Iterate the loop till b==0
+        {
+            if ((b & 01) != 0) // Logical ANDing of the value of b with 01
+            {
+                result = result + a; // Update the result with the new value of a.
+            }
+            a <<= 1;              // Left shifting the value contained in 'a' by 1.
+            b >>= 1;             // Right shifting the value contained in 'b' by 1.
+        }
+        return result;
+    }
+
+    public static int add(int n1, int n2) {
+        int x = n1, y = n2;
+        int xor, and, temp;
+        and = x & y;
+        xor = x ^ y;
+
+        while (and != 0) {
+            and <<= 1;
+            temp = xor ^ and;
+            and &= xor;
+            xor = temp;
+        }
+        return xor;
+    }
+
+    public static short squareRoot(short num) {
+        short res = 0;
+        short bit = 1 << 14; // The second-to-top bit is set: 1L<<30 for long
+
+        // "bit" starts at the highest power of four <= the argument.
+        while (bit > num)
+            bit >>= 2;
+
+        while (bit != 0) {
+            if (num >= res + bit) {
+                num -= res + bit;
+                res = (short) ((res >> 1) + bit);
+            }
+            else
+                res >>= 1;
+            bit >>= 2;
+        }
+        return res;
+    }
+}
